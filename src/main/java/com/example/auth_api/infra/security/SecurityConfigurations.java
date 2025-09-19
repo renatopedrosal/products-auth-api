3a8ab75f137e8b0ba6e83c5282255b05cab85efa
Renato Pedrosa <renatopedrosaleite.rp@gmail.com>
2025-09-19 01:05:19 -0300

    fix: Ajustes na liberação das rotas para o h2-database

--- src/main/java/com/example/auth_api/infra/security/SecurityConfigurations.java
+++ src/main/java/com/example/auth_api/infra/security/SecurityConfigurations.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,12 +27,15 @@ public class SecurityConfigurations {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                //Add para liberar iframe do h2-database
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .build();
     }
 
--- target/classes/db/migration/V3__insert-initial-table.sql
+++ target/classes/db/migration/V3__insert-initial-table.sql
@@ -3,10 +3,3 @@ INSERT INTO product (id, name, price) VALUES ('p1', 'Notebook Dell', 4500);
 INSERT INTO product (id, name, price) VALUES ('p2', 'Monitor LG 24"', 950);
 INSERT INTO product (id, name, price) VALUES ('p3', 'Teclado Mecânico', 350);
 INSERT INTO product (id, name, price) VALUES ('p4', 'Mouse Gamer', 250);
-
--- Inserts iniciais para tabela users
-INSERT INTO users (id, login, password, role)
-VALUES ('11111111-1111-1111-1111-111111111111', 'admin', '{noop}admin123', 'ADMIN');
-
-INSERT INTO users (id, login, password, role)
-VALUES ('22222222-2222-2222-2222-222222222222', 'user1', '{noop}user123', 'USER');
